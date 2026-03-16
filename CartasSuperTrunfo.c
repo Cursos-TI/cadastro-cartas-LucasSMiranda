d2e998bdcae90026e7a652dbee6f97188b9a45ae
Lucas Miranda <104878497+LucasSMiranda@users.noreply.github.com>
2026-03-16 22:46:46 +0000

    ajustes de váriaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,12 +6,9 @@
 
 int main(){
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char estado[50];
-  char codigo_carta[50];
-  char nome_cidade[50];
+  char estado[50], codigo_carta[50], nome_cidade[50];
   int populacao;
-  float area;
-  float pib;
+  float area, pib;
   int ponto_turistico;
 
   // Área para entrada de dados
